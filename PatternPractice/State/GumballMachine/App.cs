0eec0156d5d59ce8487fa2ab9bc855e456418662
Michael <vaiyanzi@gmail.com>
2013-08-28 15:38:28 +0800

    clean code.

--- PatternPractice/State/GumballMachine/App.cs
+++ PatternPractice/State/GumballMachine/App.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PatternPractice.State.GumballMachine
 {
@@ -10,7 +6,7 @@ namespace PatternPractice.State.GumballMachine
     {
         public static void Excute()
         {
-            GumballMachine gumballMachine = new GumballMachine(5);
+            var gumballMachine = new GumballMachine(5);
             Console.WriteLine(gumballMachine);
 
             gumballMachine.InsertQuarter();
