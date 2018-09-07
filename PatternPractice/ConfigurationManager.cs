da6fd8f3f533f4ca8d1467cc00a8a4d32d3b1647
Michael <vaiyanzi@gmail.com>
2018-09-07 15:14:34 +0800

    Clean code.

--- PatternPractice/ConfigurationManager.cs
+++ PatternPractice/ConfigurationManager.cs
@@ -1,12 +1,10 @@
-﻿using System.Runtime.InteropServices.ComTypes;
-using PatternPractice.AbstractFactory;
-using static System.Configuration.ConfigurationSettings;
+﻿using static System.Configuration.ConfigurationManager;
 
 namespace PatternPractice
 {
     public static class ConfigManager
     {
-        public static string MethodFactoryKey=> AppSettings["FactoryMethod"];
+        public static string MethodFactoryKey => AppSettings["FactoryMethod"];
         public static string AbstractFactoryKey => AppSettings["AbstractFactory"];
         public static string BuilderKey => AppSettings["Builder"];
         public static string AdapterKey => AppSettings["Adapter"];
